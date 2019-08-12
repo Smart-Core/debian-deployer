2f5daf9b8293a1f1ab77c349c2a0e42c36ebcbfd
Artem Ryzhkov <digital_boy@mail.ru>
2019-08-12 22:20:39 +0700

    upd

--- install.sh
+++ install.sh
@@ -24,16 +24,17 @@ PHP_VERSION="73"
 #        ;;
 #esac
 
-read -p "Install Apache Web Server (N/y)" INSTALL_APACHE
+INSTALL_APACHE="0"
 
-case $INSTALL_APACHE in
-    y|Y) echo -e "${GREEN} Enable Apache ${NORMAL}"
-         INSTALL_APACHE="1"
-         ;;
-    *)   echo -e "${YELLOW} Ignore Apache ${NORMAL}"
-         INSTALL_APACHE="0"
-         ;;
-esac
+#read -p "Install Apache Web Server (N/y)" INSTALL_APACHE
+#case $INSTALL_APACHE in
+#    y|Y) echo -e "${GREEN} Enable Apache ${NORMAL}"
+#         INSTALL_APACHE="1"
+#         ;;
+#    *)   echo -e "${YELLOW} Ignore Apache ${NORMAL}"
+#         INSTALL_APACHE="0"
+#         ;;
+#esac
 
 read -p "Install PostrgeSQL (N/y)" INSTALL_POSTRGESQL
 
@@ -47,7 +48,7 @@ case $INSTALL_POSTRGESQL in
 esac
 
 apt-get install apt-utils -y
-apt-get install apt-transport-https dialog dirmngr apt-utils locales locales-all debian-archive-keyring curl -y
+apt-get install apt-transport-https dialog dirmngr apt-utils locales locales-all debian-archive-keyring sudo curl -y
 
 dpkg-reconfigure locales
 dpkg-reconfigure tzdata
@@ -55,54 +56,7 @@ dpkg-reconfigure tzdata
 apt-get upgrade -y
 
 # @todo http://blog.programster.org/debian-8-install-php-7-1/
-if (( $DEBIAN_VERSION == 8 ))
-then
-    echo -e "${YELLOW} Jessie installing... ${NORMAL}"
-
-    # Ondrej Sury php 7.1
-    wget --quiet -O - https://packages.sury.org/php/apt.gpg | apt-key add -
-    printf "deb https://packages.sury.org/php/ jessie main" > /etc/apt/sources.list.d/php7.1.list
-
-    # Dotdeb 7.0
-    wget --quiet -O - http://www.dotdeb.org/dotdeb.gpg | apt-key add -
-    printf "deb http://packages.dotdeb.org jessie all\ndeb-src http://packages.dotdeb.org jessie all\ndeb http://mirror.nl.leaseweb.net/dotdeb/ jessie all\ndeb-src http://mirror.nl.leaseweb.net/dotdeb/ jessie all" > /etc/apt/sources.list.d/dotdeb.list
-
-    # Nginx
-    wget --quiet -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
-    printf "deb http://nginx.org/packages/mainline/debian/ jessie nginx\ndeb-src http://nginx.org/packages/mainline/debian/ jessie nginx" > /etc/apt/sources.list.d/nginx.list
-
-    # Java
-    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
-    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EA8CACC073C3DB2A
-    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C2518248EEA14886
-    printf "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main\ndeb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" > /etc/apt/sources.list.d/java.list
-
-    # Varnish
-    wget --quiet -O - https://repo.varnish-cache.org/debian/GPG-key.txt | apt-key add -
-    echo "deb https://repo.varnish-cache.org/debian/ wheezy varnish-3.0" > /etc/apt/sources.list.d/varnish-cache.list
-
-    # PostgreSQL
-    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-    apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 7FCC7D46ACCC4CF8
-    echo "deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main" > /etc/apt/sources.list.d/postgresql.list
-
-    # Maria DB
-    apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
-    printf "deb http://mirror.mephi.ru/mariadb/repo/10.0/debian jessie main\ndeb-src http://mirror.mephi.ru/mariadb/repo/10.0/debian jessie main" > /etc/apt/sources.list.d/mariadb.list
-
-    # MongoDB
-    apt-key adv --keyserver keyserver.ubuntu.com --recv 9ECBEC467F0CEB10
-    apt-key adv --keyserver keyserver.ubuntu.com --recv D68FA50FEA312927
-    apt-key adv --keyserver keyserver.ubuntu.com --recv EA312927
-    apt-key adv --keyserver keyserver.ubuntu.com --recv BC711F9BA15703C6
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-    echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/4.0 main" > /etc/apt/sources.list.d/mongodb.list
-
-    # Subversion 1.9
-    wget -q http://opensource.wandisco.com/wandisco-debian.gpg -O- | apt-key add -
-    echo "deb http://staging.opensource.wandisco.com/debian jessie svn19" > /etc/apt/sources.list.d/subversion.list
-
-elif (( $DEBIAN_VERSION == 9 ))
+if (( $DEBIAN_VERSION == 9 ))
 then
     echo -e "${YELLOW} Debian 9 'Stretch' installing... ${NORMAL}"
 
@@ -143,17 +97,21 @@ then
 #    wget -q http://opensource.wandisco.com/wandisco-debian.gpg -O- | apt-key add -
 #    echo "deb http://staging.opensource.wandisco.com/debian stretch svn112" > /etc/apt/sources.list.d/subversion.list
 
-elif (( $DEBIAN_VERSION == 10 ))
+elif (( $RELEASE == 'buster' ))
 then
     echo -e "${YELLOW} Debian 10 'Buster' installing... ${NORMAL}"
 
+    # Docker
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
     # Ondrej Sury php 7.3
-#    wget --quiet -O - https://packages.sury.org/php/apt.gpg | apt-key add -
-#    printf "deb https://packages.sury.org/php/ stretch main" > /etc/apt/sources.list.d/php7.3.list
+    wget --quiet -O - https://packages.sury.org/php/apt.gpg | apt-key add -
+    printf "deb https://packages.sury.org/php/ ${RELEASE} main" > /etc/apt/sources.list.d/php7.3.list
 
     # Dotdeb
-#    wget --quiet -O - http://www.dotdeb.org/dotdeb.gpg | apt-key add -
-#    printf "deb http://packages.dotdeb.org stretch all\ndeb-src http://packages.dotdeb.org stretch all\ndeb http://mirror.nl.leaseweb.net/dotdeb/ stretch all\ndeb-src http://mirror.nl.leaseweb.net/dotdeb/ stretch all" > /etc/apt/sources.list.d/dotdeb.list
+    wget --quiet -O - http://www.dotdeb.org/dotdeb.gpg | apt-key add -
+    printf "deb http://packages.dotdeb.org ${RELEASE} all\ndeb-src http://packages.dotdeb.org ${RELEASE} all\ndeb http://mirror.nl.leaseweb.net/dotdeb/ ${RELEASE} all\ndeb-src http://mirror.nl.leaseweb.net/dotdeb/ ${RELEASE} all" > /etc/apt/sources.list.d/dotdeb.list
 
     # Nginx
     wget --quiet -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
@@ -184,6 +142,20 @@ then
 #    wget -q http://opensource.wandisco.com/wandisco-debian.gpg -O- | apt-key add -
 #    echo "deb http://staging.opensource.wandisco.com/debian stretch svn112" > /etc/apt/sources.list.d/subversion.list
 
+    # elasticsearch
+    #wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+    #echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elasticsearch.list
+
+    # RabbitMQ
+    wget -O - "https://packagecloud.io/rabbitmq/rabbitmq-server/gpgkey" | apt-key add -
+    #wget -qO - http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | apt-key add -
+    #apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B73A36E6026DFCA
+    curl -fsSL https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc | apt-key add -
+    echo "deb https://dl.bintray.com/rabbitmq-erlang/debian ${RELEASE} erlang-21.x\ndeb https://dl.bintray.com/rabbitmq/debian ${RELEASE} main" > /etc/apt/sources.list.d/rabbitmq.list
+
+    # Ruby Version Manager
+    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
     # Yarn
     curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
@@ -193,11 +165,6 @@ else
     exit
 fi
 
-
-# elasticsearch
-#wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-#echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elasticsearch.list
-
 # Apache Cassandra
 #apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 749D6EEC0353B12C
 #gpg --keyserver pgp.mit.edu --recv-keys F758CE318D77295D
@@ -206,29 +173,21 @@ fi
 #gpg --export --armor 2B5C1B00 | apt-key add -
 #printf "deb http://www.apache.org/dist/cassandra/debian 20x main\ndeb-src http://www.apache.org/dist/cassandra/debian 20x main" > /etc/apt/sources.list.d/cassandra.list
 
-# RabbitMQ
-#wget -qO - http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | apt-key add -
-#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B73A36E6026DFCA
-#echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
-
-# Ruby Version Manager
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-
 # NodeJS
 #  in this step apt-get update will executes automatically
 wget -qO- https://deb.nodesource.com/setup_12.x | bash -
 
 # Базовый софт
-apt-get install net-tools gnupg -y
+apt-get install net-tools gnupg gnupg2 ca-certificates -y
 #apt-get install libmyodbc python-software-properties monit -y
 apt-get install colordiff mc htop gcc g++ make git curl rcconf p7zip-full zip dnsutils software-properties-common -y
-apt-get install acl bash-completion fail2ban resolvconf subversion sudo ntp imagemagick p7zip tree -y
+apt-get install acl bash-completion fail2ban resolvconf subversion ntp imagemagick p7zip tree -y
 apt-get install libedit-dev libevent-dev libcurl4-openssl-dev automake1.1 libncurses-dev libpcre3-dev pkg-config python-docutils -y
 apt-get install libodbc1 fcgiwrap libgd-tools snmp mailutils yarn -y
 #apt-get install oracle-java12-installer -y
 apt-get install default-jdk -y
 #apt-get install elasticsearch -y
-#apt-get install rabbitmq-server -y
+apt-get install rabbitmq-server -y --fix-missing
 
 # БД
 apt-get install redis-server -y
@@ -247,6 +206,7 @@ fi
 #apt-get install cassandra -y
 #apt-get install mongodb-org php5-mongo -y
 #apt-get install mongodb php5-mongo -y
+apt-get install docker-ce
 apt-get install nodejs -y
 
 # Web servers
@@ -262,53 +222,25 @@ fi
 apt-get install nginx memcached mcrypt uw-mailutils -y
 chmod 0777 /var/log/nginx
 
-if (( $PHP_VERSION == 71 ))
-then
-    # PHP 7.1
-    apt-get install php php-cli php-dev php-fpm php-pear php-gd php-intl php-curl php-gmp php-mcrypt php-bz2 php-mbstring -y
-    apt-get install php-snmp php-xmlrpc php7.1-xsl php7.1-mysql php-pgsql php-tidy php7.1-redis php-imap php-zip php-bcmath -y
-    apt-get install php7.1-apcu php7.1-geoip php7.1-imagick php7.1-sqlite3 php7.1-ssh2 php7.1-memcached -y
+# PHP 7.3
+apt-get install php php-cli php-dev php-fpm php-pear php-gd php-intl php-curl php-gmp php-bz2 php-mbstring -y
+apt-get install php-snmp php-xmlrpc php-mysql php-pgsql php-tidy php-redis php-imap php-zip php-bcmath -y
+apt-get install php-apcu php-geoip php-imagick php-sqlite3 php-ssh2 php-memcached -y
 
-    ln -s /etc/php/7.1/global.ini /etc/php/7.1/apache2/conf.d/00-global.ini
-    ln -s /etc/php/7.1/global.ini /etc/php/7.1/cli/conf.d/00-global.ini
-    ln -s /etc/php/7.1/global.ini /etc/php/7.1/fpm/conf.d/00-global.ini
+ln -s /etc/php/7.3/global.ini /etc/php/7.3/apache2/conf.d/00-global.ini
+ln -s /etc/php/7.3/global.ini /etc/php/7.3/cli/conf.d/00-global.ini
+ln -s /etc/php/7.3/global.ini /etc/php/7.3/fpm/conf.d/00-global.ini
 
-    ln -s /etc/php/7.1/php-apache.ini /etc/php/7.1/apache2/conf.d/01-php-apache.ini
-    ln -s /etc/php/7.1/php-cli.ini /etc/php/7.1/cli/conf.d/01-php-cli.ini
-    ln -s /etc/php/7.1/php-fpm.ini /etc/php/7.1/fpm/conf.d/01-php-fpm.ini
+ln -s /etc/php/7.3/php-apache.ini /etc/php/7.3/apache2/conf.d/01-php-apache.ini
+ln -s /etc/php/7.3/php-cli.ini /etc/php/7.3/cli/conf.d/01-php-cli.ini
+ln -s /etc/php/7.3/php-fpm.ini /etc/php/7.3/fpm/conf.d/01-php-fpm.ini
 
-    a2enmod php7.1
-    #a2enmod proxy_fcgi setenvif
-    #a2enconf php7.0-fpm
+#a2enmod php7.3
+#a2enmod proxy_fcgi setenvif
+#a2enconf php7.0-fpm
 
-    /etc/init.d/php7.1-fpm restart
-    update-rc.d php7.1-fpm defaults
-
-elif (( $PHP_VERSION == 73 ))
-then
-    # PHP 7.3
-
-    # php-mcrypt php-xsl
-
-    apt-get install php php-cli php-dev php-fpm php-pear php-gd php-intl php-curl php-gmp php-bz2 php-mbstring -y
-    apt-get install php-snmp php-xmlrpc php-mysql php-pgsql php-tidy php-redis php-imap php-zip php-bcmath -y
-    apt-get install php-apcu php-geoip php-imagick php-sqlite3 php-ssh2 php-memcached -y
-
-    ln -s /etc/php/7.3/global.ini /etc/php/7.3/apache2/conf.d/00-global.ini
-    ln -s /etc/php/7.3/global.ini /etc/php/7.3/cli/conf.d/00-global.ini
-    ln -s /etc/php/7.3/global.ini /etc/php/7.3/fpm/conf.d/00-global.ini
-
-    ln -s /etc/php/7.3/php-apache.ini /etc/php/7.3/apache2/conf.d/01-php-apache.ini
-    ln -s /etc/php/7.3/php-cli.ini /etc/php/7.3/cli/conf.d/01-php-cli.ini
-    ln -s /etc/php/7.3/php-fpm.ini /etc/php/7.3/fpm/conf.d/01-php-fpm.ini
-
-#    a2enmod php7.3
-    #a2enmod proxy_fcgi setenvif
-    #a2enconf php7.0-fpm
-
-    /etc/init.d/php7.3-fpm restart
-    update-rc.d php7.3-fpm defaults
-fi
+/etc/init.d/php7.3-fpm restart
+update-rc.d php7.3-fpm defaults
 
 mkdir /var/lib/php
 mkdir /var/lib/php/sessions
@@ -324,16 +256,17 @@ then
     cp -R common/etc / -v
     cp -R common/usr / -v
     cp -R common/root / -v
-    cp -R "debian$DEBIAN_VERSION/etc" / -v
+    cp -R "debian-$RELEASE/etc" / -v
     cp -v create-apache-vhost /usr/local/bin/create-apache-vhost
     cp -v create-nginx-vhost /usr/local/bin/create-nginx-vhost
     cp -v create-symfony-nginx-vhost /usr/local/bin/create-symfony-nginx-vhost
+    cp -v create-symfony-old-nginx-vhost /usr/local/bin/create-symfony-old-nginx-vhost
     chmod 0755 /usr/local/bin/create-nginx-vhost
     chmod 0755 /usr/local/bin/create-symfony-nginx-vhost
 fi
 
 # Cache
-apt-get install varnish -y
+#apt-get install varnish -y
 
 # Composer
 curl -skS https://getcomposer.org/installer | php
@@ -366,14 +299,15 @@ apt-get autoremove -y
 mkdir /var/www
 
 # Ruby
-    ## https://www.digitalocean.com/community/tutorials/how-to-install-ruby-on-rails-on-an-debian-7-0-wheezy-vps-using-rvm
-#curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-#curl -L https://get.rvm.io | bash -s stable --ruby
-#source /etc/profile.d/rvm.sh
-    ## source /usr/local/rvm/scripts/rvm
-    ## apt-get install ruby ruby-dev
-#gem install capistrano
-#gem install capistrano-composer
-#gem install capistrano-maintenance
-#gem install capistrano-symfony
-#gem update
+## https://www.digitalocean.com/community/tutorials/how-to-install-ruby-on-rails-on-an-debian-7-0-wheezy-vps-using-rvm
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -L https://get.rvm.io | bash -s stable --ruby
+source /etc/profile.d/rvm.sh
+source /usr/local/rvm/scripts/rvm
+apt-get install ruby ruby-dev
+gem install capistrano
+gem install capistrano-composer
+gem install capistrano-maintenance
+gem install capistrano-symfony
+gem update
