47bf3f99a475c4b9eace2635f711406c15c1051e
artem <digital_boy@mail.ru>
2020-02-20 23:33:50 +0700

    upd

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt install lsb_release -y
+
 NORMAL='\033[0m'     #  ${NORMAL}
 RED='\033[0;31m'     #  ${RED}
 GREEN='\033[0;32m'   #  ${GREEN}
@@ -47,8 +49,30 @@ case $INSTALL_POSTRGESQL in
         ;;
 esac
 
+read -p "Install Ruby (N/y)" INSTALL_RUBY
+
+case INSTALL_RUBY in
+    y|Y)echo -e "${GREEN} Enable Ruby ${NORMAL}"
+        INSTALL_RUBY="1"
+        ;;
+    *)  echo -e "${YELLOW} Ignore Ruby ${NORMAL}"
+        INSTALL_RUBY="0"
+        ;;
+esac
+
+read -p "Install Docker (N/y)" INSTALL_DOCKER
+
+case INSTALL_DOCKER in
+    y|Y)echo -e "${GREEN} Enable Docker ${NORMAL}"
+        INSTALL_DOCKER="1"
+        ;;
+    *)  echo -e "${YELLOW} Ignore Docker ${NORMAL}"
+        INSTALL_DOCKER="0"
+        ;;
+esac
+
 apt-get install apt-utils -y
-apt-get install apt-transport-https dialog dirmngr apt-utils locales locales-all debian-archive-keyring sudo curl -y
+apt-get install apt-transport-https dialog dirmngr apt-utils locales locales-all debian-archive-keyring sudo curl software-properties-common wget -y
 
 dpkg-reconfigure locales
 dpkg-reconfigure tzdata
@@ -104,7 +128,7 @@ then
     # Docker
     curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
     apt-key fingerprint 0EBFCD88
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian ${RELEASE} stable"
 
     # Ondrej Sury php 7.3
     wget --quiet -O - https://packages.sury.org/php/apt.gpg | apt-key add -
@@ -136,8 +160,8 @@ then
 #    printf "deb http://mirror.mephi.ru/mariadb/repo/10.0/debian jessie main\ndeb-src http://mirror.mephi.ru/mariadb/repo/10.0/debian jessie main" > /etc/apt/sources.list.d/mariadb.list
 
     # MongoDB
-#    wget -q https://www.mongodb.org/static/pgp/server-4.0.asc -O - | apt-key add -
-#    echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" > /etc/apt/sources.list.d/mongodb.list
+    wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+    echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
     # Subversion 1.12
 #    wget -q http://opensource.wandisco.com/wandisco-debian.gpg -O- | apt-key add -
@@ -176,19 +200,21 @@ fi
 
 # NodeJS
 #  in this step apt-get update will executes automatically
-wget -qO- https://deb.nodesource.com/setup_12.x | bash -
+wget -qO- https://deb.nodesource.com/setup_13.x | bash -
 
 # Базовый софт
 apt-get install net-tools gnupg gnupg2 ca-certificates -y
 #apt-get install libmyodbc python-software-properties monit -y
-apt-get install colordiff mc htop gcc g++ make git curl rcconf p7zip-full zip dnsutils software-properties-common -y
+apt-get install colordiff mc htop gcc g++ make git curl rcconf p7zip-full zip dnsutils -y
 apt-get install acl bash-completion fail2ban resolvconf subversion ntp imagemagick p7zip tree -y
 apt-get install libedit-dev libevent-dev libcurl4-openssl-dev automake1.1 libncurses-dev libpcre3-dev pkg-config python-docutils -y
 apt-get install libodbc1 fcgiwrap libgd-tools snmp mailutils yarn -y
-#apt-get install oracle-java12-installer -y
-apt-get install default-jdk -y
-#apt-get install elasticsearch -y
 apt-get install rabbitmq-server -y --fix-missing
+apt-get install nodejs -y
+#apt-get install default-jdk -y
+
+#apt-get install oracle-java12-installer -y
+#apt-get install elasticsearch -y
 
 # БД
 apt-get install redis-server -y
@@ -197,24 +223,29 @@ apt-get install redis-server -y
 # https://andreyex.ru/operacionnaya-sistema-debian/kak-ustanovit-mariadb-na-debian-10/
 apt-get install mariadb-server -y
 mysql_secure_installation
-#apt-get install mysql-server mysql-client -y
 
+#apt-get install mysql-server mysql-client -y
+#apt-get install cassandra -y
+#apt-get install mongodb-org php7.3-mongo -y
+#apt-get install mongodb php5-mongo -y # устаревший
+
+# PostgreSQL
 if (( $INSTALL_POSTRGESQL == 1 ))
 then
     apt-get install postgresql postgresql-contrib odbc-postgresql -y
     update-rc.d postgresql defaults
 fi
 
-#apt-get install cassandra -y
-#apt-get install mongodb-org php5-mongo -y
-#apt-get install mongodb php5-mongo -y
-apt-get install docker-ce docker-ce-cli containerd.io
-apt-get install nodejs -y
+# Docker
+if (( $INSTALL_DOCKER == 1 ))
+then
+    apt-get install docker-ce docker-ce-cli containerd.io
+fi
 
-# Web servers
+# Apache
 if (( $INSTALL_APACHE == 1 ))
 then
-    apt-get install apache2 apache2-mpm-prefork libapache2-mod-rpaf  -y
+    apt-get install -y apache2 apache2-mpm-prefork libapache2-mod-rpaf
 
     rm /etc/apache2/sites-enabled/000-default
     rm /etc/apache2/sites-enabled/000-default.conf
@@ -225,9 +256,13 @@ apt-get install nginx memcached mcrypt uw-mailutils -y
 chmod 0777 /var/log/nginx
 
 # PHP 7.3
-apt-get install php php-cli php-dev php-fpm php-pear php-gd php-intl php-curl php-gmp php-bz2 php-mbstring -y
-apt-get install php-snmp php-xmlrpc php-mysql php-pgsql php-tidy php-redis php-imap php-zip php-bcmath -y
-apt-get install php-apcu php-geoip php-imagick php-sqlite3 php-ssh2 php-memcached -y
+apt install -y php7.3 php7.3-fpm php7.3-cli php7.3-dev php7.3-common php7.3-apcu php7.3-mysql php7.3-pgsql php7.3-sqlite3
+apt install -y php7.3-gmp php7.3-gd php7.3-bcmath php7.3-curl php7.3-intl php7.3-mbstring php7.3-bz2 php7.3-xml php7.3-zip
+apt install -y php7.3-snmp php7.3-xmlrpc php7.3-tidy php7.3-redis php7.3-imap php7.3-geoip php7.3-imagick php7.3-ssh2 php7.3-memcached
+
+#apt-get install php php-cli php-dev php-fpm php-pear php-gd php-intl php-curl php-gmp php-bz2 php-mbstring -y
+#apt-get install php-snmp php-xmlrpc php-mysql php-pgsql php-tidy php-redis php-imap php-zip php-bcmath -y
+#apt-get install php-apcu php-geoip php-imagick php-sqlite3 php-ssh2 php-memcached -y
 
 ln -s /etc/php/7.3/global.ini /etc/php/7.3/apache2/conf.d/00-global.ini
 ln -s /etc/php/7.3/global.ini /etc/php/7.3/cli/conf.d/00-global.ini
@@ -236,10 +271,6 @@ ln -s /etc/php/7.3/global.ini /etc/php/7.3/fpm/conf.d/00-global.ini
 ln -s /etc/php/7.3/php-apache.ini /etc/php/7.3/apache2/conf.d/01-php-apache.ini
 ln -s /etc/php/7.3/php-cli.ini /etc/php/7.3/cli/conf.d/01-php-cli.ini
 ln -s /etc/php/7.3/php-fpm.ini /etc/php/7.3/fpm/conf.d/01-php-fpm.ini
-
-#a2enmod php7.3
-#a2enmod proxy_fcgi setenvif
-#a2enconf php7.0-fpm
 
 /etc/init.d/php7.3-fpm restart
 update-rc.d php7.3-fpm defaults
@@ -292,6 +323,10 @@ apt-get clean
 
 if (( $INSTALL_APACHE == 1 ))
 then
+    #a2enmod php7.3
+    #a2enmod proxy_fcgi setenvif
+    #a2enconf php7.0-fpm
+
     /etc/init.d/apache2 restart
 else
     /etc/init.d/apache2 stop
@@ -303,14 +338,17 @@ mkdir /var/www
 
 # Ruby
 ## https://www.digitalocean.com/community/tutorials/how-to-install-ruby-on-rails-on-an-debian-7-0-wheezy-vps-using-rvm
-gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-curl -L https://get.rvm.io | bash -s stable --ruby
-source /etc/profile.d/rvm.sh
-source /usr/local/rvm/scripts/rvm
-apt-get install ruby ruby-dev
-gem install capistrano
-gem install capistrano-composer
-gem install capistrano-maintenance
-gem install capistrano-symfony
-gem update
+if (( $INSTALL_RUBY == 1 ))
+then
+    gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+    curl -L https://get.rvm.io | bash -s stable --ruby
+    source /etc/profile.d/rvm.sh
+    source /usr/local/rvm/scripts/rvm
+    apt-get install ruby ruby-dev
+    gem install capistrano
+    gem install capistrano-composer
+    gem install capistrano-maintenance
+    gem install capistrano-symfony
+    gem update
+fi
