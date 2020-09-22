5d25db7fea8967f6d7524fa943c6caef7643ce01
artem <digital_boy@mail.ru>
2020-09-22 16:55:22 +0700

    fix

--- install_light_docker.sh
+++ install_light_docker.sh
@@ -39,7 +39,7 @@ dpkg-reconfigure tzdata
 apt-get update
 
 apt-get install net-tools gnupg gnupg2 ca-certificates -y
-apt-get install acl colordiff curl fail2ban htop make mc mlocate sudo tmux zip -y
+apt-get install acl colordiff curl fail2ban htop make mc mlocate sudo supervisor tmux zip -y
 apt-get install nginx -y
 
 # https://debian.pkgs.org/10/debian-main-amd64/mlocate_0.26-3_amd64.deb.html
