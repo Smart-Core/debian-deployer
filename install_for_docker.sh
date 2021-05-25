1be1b1290b8d4bd6cfa0f6809c6c55e410ab5bdc
artem <you@mail.com>
2021-05-25 17:48:46 +0700

    upd

--- install_for_docker.sh
+++ install_for_docker.sh
@@ -69,7 +69,7 @@ apt-get install nodejs -y
 
 # Docker
 apt-get install -y docker-ce docker-ce-cli containerd.io
-curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 apt-get install nginx mcrypt -y
