327fa27f44a06332b32bcd289baf5aa21fe43f16
artemdigi <artem.digi@gmail.com>
2021-12-30 23:36:00 +0700

    fix

--- install_light_docker.sh
+++ install_light_docker.sh
@@ -4,7 +4,7 @@ SCRIPT_START_SECONDS=$(date +%s)
 SCRIPT_START_DATE=$(date +%T)
 
 # Указать версии компонентов докера
-DOCKER_COMPOSE=2.1.1
+DOCKER_COMPOSE=2.2.2
 #DOCKER_CE=docker-ce_19.03.12~3-0~debian-buster_amd64.deb
 #DOCKER_CE_CLI=docker-ce-cli_19.03.12~3-0~debian-buster_amd64.deb
 #CONTAINERD_IO=containerd.io_1.2.13-2_amd64.deb
@@ -13,41 +13,20 @@ NORMAL='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
-DEBIAN_VERSION=$(cat /etc/debian_version | head -c 1)
 RELEASE=$(lsb_release -cs)
 
 tput sgr0
 
 apt install wget curl software-properties-common dirmngr apt-transport-https lsb-release ca-certificates -y
 
-if (( $DEBIAN_VERSION == 9 ))
-then
-    echo -e "${YELLOW} Debian 9 'Stretch' installing... ${NORMAL}"
+# Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian ${RELEASE} stable"
 
-    # Docker
-    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-    apt-key fingerprint 0EBFCD88
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian ${RELEASE} stable"
-
-    # Nginx
-    wget --quiet -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
-    printf "deb http://nginx.org/packages/mainline/debian/ stretch nginx\ndeb-src http://nginx.org/packages/mainline/debian/ stretch nginx" > /etc/apt/sources.list.d/nginx.list
-elif (( $RELEASE == 'buster' ))
-then
-    echo -e "${YELLOW} Debian 10 'Buster' installing... ${NORMAL}"
-
-    # Docker
-    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-    apt-key fingerprint 0EBFCD88
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian ${RELEASE} stable"
-
-    # Nginx
-    wget --quiet -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
-    printf "deb http://nginx.org/packages/mainline/debian/ buster nginx\ndeb-src http://nginx.org/packages/mainline/debian/ buster nginx" > /etc/apt/sources.list.d/nginx.list
-else
-    echo -e "${RED} BAD Debian version ${NORMAL}"
-    exit
-fi
+# Nginx
+wget --quiet -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
+printf "deb http://nginx.org/packages/mainline/debian/ ${RELEASE} nginx\ndeb-src http://nginx.org/packages/mainline/debian/ ${RELEASE} nginx" > /etc/apt/sources.list.d/nginx.list
 
 sed -i s/'# ru_RU.UTF-8 UTF-8'/'ru_RU.UTF-8 UTF-8'/g /etc/locale.gen
 locale-gen ru_RU.UTF-8
