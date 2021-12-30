193d530b7b4accbdf34734f42302b42e76d2ad9b
artemdigi <artem.digi@gmail.com>
2021-12-31 00:47:59 +0700

    add php sury repo

--- install_light_docker.sh
+++ install_light_docker.sh
@@ -28,6 +28,10 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian ${
 wget --quiet -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
 printf "deb http://nginx.org/packages/mainline/debian/ ${RELEASE} nginx\ndeb-src http://nginx.org/packages/mainline/debian/ ${RELEASE} nginx" > /etc/apt/sources.list.d/nginx.list
 
+# Ondrej Sury php
+wget --quiet -O - https://packages.sury.org/php/apt.gpg | apt-key add -
+printf "deb https://packages.sury.org/php/ ${RELEASE} main" > /etc/apt/sources.list.d/php-sury.list
+
 sed -i s/'# ru_RU.UTF-8 UTF-8'/'ru_RU.UTF-8 UTF-8'/g /etc/locale.gen
 locale-gen ru_RU.UTF-8
 localectl set-locale LANG=ru_RU.UTF-8
@@ -36,6 +40,7 @@ update-locale LANG=ru_RU.UTF-8
 dpkg-reconfigure tzdata
 
 apt update
+apt upgrade
 
 # python-certbot-nginx
 apt install acl bash-completion certbot colordiff fail2ban net-tools gnupg gnupg2 htop make mailutils mc mlocate sudo supervisor time tmux zip -y
